e8bd3af2575f20989ba3f6ac58785a8dd90f79f7
shida-zheng-ICD2O1-01 <shida.zheng@student.tdsb.on.ca>
2024-11-07 15:30:34 +0000

    11

--- PP1_6.py
+++ PP1_6.py
@@ -7,17 +7,17 @@
 
 # From input, recieve two integers from the user and add them together.  Output the result.
 def q1():
-  num1 = input("Input a number: ")
-  num2 = input("Input a number: ")
+  num1 = float(input("Input a number: "))
+  num2 = float(input("Input a number: "))
   
-  print(num1 + num2)
+  print(float(num1 + num2))
 
 # From input recieve two integers.  Output the quotient rounded down.
 
 def q2():
-  num1 = input("Input a number: ")
-  num2 = input("Input a number: ")  
-  print(num1 / num2)
+  num1 = float(input("Input a number: "))
+  num2 = float(input("Input a number: "))  
+  print(int(num1 / num2))
 
 # Output the phrase "hello Mr. Kalisz have you seen my work yet?"
 
@@ -28,8 +28,8 @@ def q3():
 # Output their result multiplied together.  Then round down to the nearest whole number
 
 def q4():
-  num1 = input("Input a number: ")
-  num2 = input("Input a number: ")
+  num1 = float(input("Input a number: "))
+  num2 = float(input("Input a number: "))
   print (int(num1 * num2))
 
 #q1()
